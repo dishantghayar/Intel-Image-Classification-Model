dd2d34eece9c294952199c87ccf1fe1ac7ae0732
Dishant Ghayar <155740340+dishantghayar@users.noreply.github.com>
2025-03-08 20:29:17 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.models import Model
+
+
+def create_model(image_size: Tuple[int, int], num_classes: int) -> Model:
+    inception_v3 = InceptionV3(
+        input_shape=(*image_size, 3), include_top=False, weights="imagenet"
+    )
+    for layer in inception_v3.layers:
+        layer.trainable = False
+
+    output = inception_v3.output
+
+    x = Flatten()(output)
+    x = Dense(1024, activation="relu")(x)
+    x = Dropout(0.2)(x)
+
+    prediction = Dense(num_classes, activation="softmax")(x)
+
+    return Model(inputs=inception_v3.input, outputs=prediction)
